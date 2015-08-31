79cefe184c4fd1d2f30018f7a2dec827c84ddc35
funkyeah <funkyeah@gmail.com>
2015-08-31 09:22:23 -0500

    moar debug statements

--- index.js
+++ index.js
@@ -59,8 +59,10 @@ Filepicker.prototype.stat = function(url, options, callback) {
                 return;
              }
         } else {
+            console.log('returnJSON);
             returnJson = body;
         }
+        console.log('callbacked);
         callback(returnJson);
         return returnJson;
     });
